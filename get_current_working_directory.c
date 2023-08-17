d68f8ecb720a04fdf83afea8345ed991d853f484
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-08-17 05:51:08 -0700

    cwd

---
+++ get_current_working_directory.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+char *get_current_working_directory()
+{
+	static char cwd[1024];
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+	{
+		return cwd;
+	}
+	else
+	{
+		return NULL;
+	}
+}
