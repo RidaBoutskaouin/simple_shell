245792ef6ad40dfe78255ee47bf42519506ad43b
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-08-17 06:00:58 -0700

    updating main header

--- main.h
+++ main.h
@@ -4,5 +4,8 @@
 bool is_absolute_path(const char *path);
 char *get_env(const char *name);
 char *full_command(char *user_cmd);
+char _getchar(void);
+ssize_t _getline(char **line, size_t *lenth, FILE *stream);
+char *get_current_working_directory();
 
 #endif
