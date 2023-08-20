04dcb6dd2ddb5fe1de11f81e8402d94fb031cc23
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-08-20 17:32:40 +0100

    print error function

--- print_error.c
+++ print_error.c
@@ -2,7 +2,7 @@
 
 
 
-void print_error(char **av, int cnt, char *line)
+void print_error(char **av, int cnt, char *line, bool isabs)
 {
     char count_str = cnt + '0';
 
@@ -11,6 +11,9 @@ void print_error(char **av, int cnt, char *line)
     write(STDERR_FILENO, &count_str, 1);
     write(STDERR_FILENO, ": ", 2);
     write(STDERR_FILENO, line, strlen(line));
-    write(STDERR_FILENO, ": ", 2);
-    perror("");
+
+    if (isabs == false)
+        write(STDERR_FILENO, ": command not found\n", 20);
+    else if (isabs == true)
+        write(STDERR_FILENO, ": No such file or directory\n", 28);
 }
