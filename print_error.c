f59c06b3b20e9372152462525064e64ff938a7c6
RidaBoutskaouin <65875595+RidaBoutskaouin@users.noreply.github.com>
2023-08-23 12:27:10 +0100

    Update print_error.c

--- print_error.c
+++ print_error.c
@@ -12,11 +12,15 @@ void print_error(char **av, int cnt, char *line, bool isabs)
     bytes_written = write(STDERR_FILENO, &count_str, 1);
     bytes_written = write(STDERR_FILENO, ": ", 2);
     bytes_written = write(STDERR_FILENO, line, strlen(line));
-    if (bytes_written = -1)
+    if (bytes_written == -1)
     {};
 
     if (isabs == false)
-        (void)write(STDERR_FILENO, ": command not found\n", 20);
+        bytes_written = write(STDERR_FILENO, ": command not found\n", 20);
+        if (bytes_written == -1)
+        {};
     else if (isabs == true)
-        (void)write(STDERR_FILENO, ": No such file or directory\n", 28);
+        bytes_written = write(STDERR_FILENO, ": No such file or directory\n", 28);
+        if (bytes_written == -1)
+        {};
 }
