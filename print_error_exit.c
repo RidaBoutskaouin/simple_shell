3969e7eeef55dfa872d8b070416ee73ecf414c6b
RidaBoutskaouin <65875595+RidaBoutskaouin@users.noreply.github.com>
2023-08-23 12:27:31 +0100

    Update print_error_exit.c

--- print_error_exit.c
+++ print_error_exit.c
@@ -20,6 +20,6 @@ void print_error_exit(char **av, int cnt, char **words)
 	bytes_written = write(STDERR_FILENO, ": exit: ", 8);
 	bytes_written = write(STDERR_FILENO, words[1], strlen(words[1]));
 	bytes_written = write(STDERR_FILENO, ": numeric argument required\n", 28);
-	if (bytes_written = -1)
+	if (bytes_written == -1)
 	{};
 }
